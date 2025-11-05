78d7fb6914bf41194ab068e2bc3ab569684f3891
Leon Feller <Leon.Feller@icloud.com>
2025-11-05 15:12:41 +0000

    update

--- models/intermediate/int_orders_operational.sql
+++ models/intermediate/int_orders_operational.sql
@@ -1,16 +1,12 @@
-WITH orders_data AS (SELECT 
-    orders_id
-    , CAST(date_date AS DATE) AS date_date
-    , ROUND(SUM(revenue),2) as revenue
-    , ROUND(SUM(quantity),2) AS quantity
-    , ROUND(SUM(purchase_price * quantity),2) AS purchase_price
-FROM {{ ref("stg_raw__sales")}}
-LEFT JOIN {{ ref("stg_raw__product")}}
-USING(products_id)
-GROUP BY orders_id, date_date
-ORDER BY orders_id DESC)
-
-SELECT*
-FROM orders_data
-LEFT JOIN {{ ref("stg_raw__ship")}}
+SELECT 
+orders_id
+    , date_date
+    , revenue
+    , quantity
+    , purchase_price
+    , margin
+    , ROUND((margin + shipping_fee - logcost - ship_cost), 2) AS operational_margin 
+FROM {{ ref("int_orders_margin") }}
+LEFT JOIN {{ ref("stg_raw__ship") }}
 USING(orders_id)
+ORDER BY orders_id DESC
